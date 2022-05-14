70d0e1c902eecedf7f91eca534e6341fea7aabae
aastha <aastah.shreya1@gmail.com>
2022-05-15 01:18:31 +0530

    import error fixes

--- tlcoin/tlcoin/settings.py
+++ tlcoin/tlcoin/settings.py
@@ -16,7 +16,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from celery.schedule import crontab
+from celery.schedules import crontab
 
 
 # Quick-start development settings - unsuitable for production
