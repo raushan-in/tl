2bbab92c338e18f9e0c97327ff766ae047217511
aastha <aastah.shreya1@gmail.com>
2022-05-14 17:21:23 +0530

    notification model added

--- tlcoin/notification/models.py
+++ tlcoin/notification/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# List of coins
+class PriceThresholdNotifications(models.Model):
+    email = models.EmailField()
+    lower_threshold_flag = models.BooleanField()
+    upper_threshold_flag = models.BooleanField()
+    coin_id = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = "price notifications"
+
+    def __str__(self):
+            return f'{self.email} - {self.created_at}'
