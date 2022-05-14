237a3410358fe5d3c22a65f5aaf1f84ddb96de64
aastha <aastah.shreya1@gmail.com>
2022-05-14 09:53:39 +0530

    extracting coin data from external source

--- tlcoin/price_tracker/tasks.py
+++ tlcoin/price_tracker/tasks.py
@@ -1,7 +1,19 @@
 from __future__ import absolute_import, unicode_literals
 
+import os
+
+import requests
 from celery import shared_task
 
+COIN_SOURCE_URI = os.environ.get("COIN_SOURCE_URI", "https://api.coingecko.com/api/v3")
+
 @shared_task
-def add(x, y):
-    return x + y
+def refresh_price(coin="bitcoin", vs_currency="USD", seconds=40):
+    '''
+    Method to get coin price-vs-Currency periodic data from external data source
+    '''
+    response=requests.get(f"{COIN_SOURCE_URI}/simple/price?ids={coin}&vs_currencies={vs_currency}&include_last_updated_at=true")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"status - {response.status_code}")
